62464f13aba9905e4453050e2ee67a1f6afc28b2
Giulia Passoni <giulia@colour-profiling.com>
2023-07-16 11:24:50 +0100

    feat: reusable single line chart.

--- src/components/LinePlot.js
+++ src/components/LinePlot.js
@@ -1,21 +1,22 @@
 import React, {useEffect, useRef} from "react";
 import {hardcodedData} from "../data/hardcodedData";
-import {randomIntFromInterval} from "../utils/calcs";
+import {extractAndSortXData} from "../utils/calcs";
 import {d3lineChart} from "../utils/d3LinePlot";
 
-const width = window.innerWidth
-const height = window.innerHeight
+const width = 500
+const height = 300
 
-const gdpLineData = []
-for (let i in hardcodedData) {
-    gdpLineData.push([hardcodedData[i].gdp, randomIntFromInterval(1,10)])
-}
+const gdpLineData = extractAndSortXData(hardcodedData, 'birthsPerYear') //note: per se, this isn't a meaningful way to represent comparison gdp data. this
+// if anything, it should be a multi-line rep of e.g. the countries' 5-y gdp data
+
+const margin = {top: 10, right: 30, bottom: 30, left: 60}
+
 export function LinePlot(){
     const svgRef = useRef(null)
 
     useEffect(() => {
         console.log(gdpLineData)
-        d3lineChart(svgRef.current,width, height, gdpLineData)
+        d3lineChart(svgRef.current,width, height, margin, gdpLineData)
     }, [gdpLineData])
     return(
         <>
--- src/utils/d3LinePlot.js
+++ src/utils/d3LinePlot.js
@@ -1,68 +1,62 @@
-import {axisBottom, axisLeft, curveCardinal, curveMonotoneX, line, ScaleLinear, scaleLinear, select} from "d3";
+import {
+    axisBottom,
+    axisLeft,
+    extent,
+    scaleLinear,
+    select,
+    line,
+    curveLinear
+} from "d3";
 
-// @ts-ignore
-export function d3lineChart(myRef,width, height, data) {
-    width = width/4
-    height = height/4
+export function d3lineChart(myRef, width, height, margin, data) {
     const svg = select(myRef)
         .attr("width", width)
         .attr("height", height)
         .attr("background", '#d3d3d3')
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")")
 
-    // setting the plot scale
-    const xScale = scaleLinear()
-        .domain([0, data.length-1])
-        .range([0, width/4])
-    const yScale  = scaleLinear()
-        .domain([0, height])
-        .range([height, 0]) // it's inverted bc we always start from top left, then going downwards
+    const x = scaleLinear()
+        // .domain(extent(data, function(d) { return d[0]})) //TODO: make d3.extent work to make the code below shorter
+        .domain([Math.min(...data.map((d)=>d[0])), Math.max(...data.map((d)=>d[0]))])
+        .range([0, width - margin.right])
 
-    const generateScaledLine= line()
-        .x((d,i)=> xScale(i))
-        // .y((d, i) => yScale(i))
-        // .y(yScale)
-        .curve(curveCardinal)
+    function xAxis(g){
+        g
+            .attr("transform", `translate(0,${height-margin.bottom})`)
+            .call(axisBottom(x));
+    }
 
+    const y  = scaleLinear()
+        .domain([0, Math.max(...data.map((d)=>d[1]))])
+        .range([height - margin.bottom, margin.top]) // it's inverted bc we always start from top left, then going downwards
 
-    svg.selectAll('.line')
+    function yAxis(g){
+        g
+            // .attr("transform", `translate(${margin.left},0)`) //this line somehow changes the domain of the axis
+            .call(axisLeft(y))
+    }
+
+    const valueline = line()
+        .curve(curveLinear)
+        .x(function(d) { return x(d[0]); })
+        .y(function(d) { return y(d[1]); });
+
+    svg.append("path")
         .data([data])
-        .join('path')
-        .attr('d', d => generateScaledLine(d))
+        .attr("class", "line")
+        .attr("d", valueline)
         .attr('fill', 'none')
         .attr('stroke', 'black')
+        .attr("stroke-linecap", 'round')
+        .attr("stroke-linejoin", 'round')
 
+    svg.append("g")
+        .attr("class", "x-axis")
+        .call(xAxis);
 
-
-    // svg.append("g")
-    //     .attr("transform", "translate(0," + 100 + ")")
-    //     .call(axisBottom(x));
-    //
-    // svg.append("g")
-    //     .call(axisLeft(y));
-    //
-    // svg.append('g')
-    //     .selectAll("dot")
-    //     .data(data)
-    //     .enter()
-    //     .append("circle")
-    //     .attr("cx", function (d) { return x(d[0]); } )
-    //     .attr("cy", function (d) { return y(d[1]); } )
-    //     .attr("r", 2)
-    //     .attr("transform", "translate(" + 100 + "," + 100 + ")")
-    //     .style("fill", "#CC0000");
-    //
-    // const myline = line()
-    //     .x(function(d) { return x(d[0]); })
-    //     .y(function(d) { return y(d[1]); })
-    //     .curve(curveMonotoneX)
-//-----------------
-    // svg.append("path")
-    //     .datum(data)
-    //     .attr("class", "line")
-    //     .attr("transform", "translate(" + 100 + "," + 100 + ")")
-    //     .attr("d", myline)
-    //     .style("fill", "none")
-    //     .style("stroke", "#CC0000")
-    //     .style("stroke-width", "2");
-
+    svg.append("g")
+        .attr("class", "y-axis")
+        .call(yAxis);
 }
