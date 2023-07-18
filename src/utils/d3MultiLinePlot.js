af3f2b4cdafcaff322c67cd6df6ba1397cdea94f
Giulia Passoni <giulia@colour-profiling.com>
2023-07-18 09:04:20 +0100

    feat: implemented multiline graph, not yet working.

--- src/utils/d3MultiLinePlot.js
+++ src/utils/d3MultiLinePlot.js
@@ -1,5 +1,5 @@
 import * as d3 from "d3"
-import {axisBottom, axisLeft, curveLinear, line, scaleLinear, select, group, extent} from "d3";
+import {axisBottom, axisLeft, curveLinear, line, scaleLinear, select, group, extent, timeParse, scaleTime, scaleOrdinal, schemeCategory10} from "d3";
 
 export function d3MultiLineChart(myRef, width, height, margin, data, variableName) {
 
@@ -12,45 +12,52 @@ export function d3MultiLineChart(myRef, width, height, margin, data, variableNam
         .attr("transform",
             "translate(" + margin.left + "," + margin.top + ")")
 
-    // const x = scaleLinear()
-    //     .domain(extent(data, function(d) { return d[0]}))
-    //     .range([0, width - margin.right])
-    //
-    // function xAxis(g){
-    //     g
-    //         .attr("transform", `translate(0,${height-margin.bottom})`)
-    //         .call(axisBottom(x));
-    // }
-    //
-    // const y  = scaleLinear()
-    //     .domain(extent(data, function(d) { return d[1]}))
-    //     .range([height - margin.bottom, margin.top]) // it's inverted bc we always start from top left, then going downwards
-    //
-    // function yAxis(g){
-    //     g
-    //         // .attr("transform", `translate(${margin.left},0)`) //this line somehow changes the domain of the axis
-    //         .call(axisLeft(y))
-    // }
+    console.log(data)
+    const xScale = scaleTime()
+        .domain(extent(data[0].gdp5yearsData, function(d) { return d.year}))
+        .range([0, width - margin.right])
 
-    // const valueline = line()
-    //     .curve(curveLinear)
-    //     .x(function(d) { return x(d[0]); })
-    //     .y(function(d) { return y(d[1]); });
-    //
+    const xAxis = axisBottom(xScale).ticks(5);
+
+    const yScale  = scaleLinear()
+        .domain(extent(data[0].gdp5yearsData, function(d) { return d.gdpValue}))
+        .range([height - margin.bottom, margin.top]) // it's inverted bc we always start from top left, then going downwards
+
+    const yAxis = axisLeft(yScale).ticks(5);
+
+    let color = scaleOrdinal(schemeCategory10);
+
+    let myline = line()
+        .x((d)=> { xScale(d.year); })
+        .y((d)=> { yScale(d.gdpValue); });
+
+    let lines = svg.append('g')
+        .attr('class', 'lines');
+
+    lines.selectAll('.line-group')
+        .data([data]).enter()
+        .append('g')
+        .attr('class', 'line-group')
+        .append('path')
+        .attr('class', 'line')
+        // TODO: fix line below to render graph correctly
+        .attr('d', d => myline(d.values))
+        .style('stroke', (d, i) => color(i))
+
     // svg.append("path")
     //     .data([data])
     //     .attr("class", "line")
     //     .attr("d", valueline)
     //     .attr('fill', 'none')
     //     .attr('stroke', 'black')
-    //     .attr("stroke-linecap", 'round')
-    //     .attr("stroke-linejoin", 'round')
 
-    // svg.append("g")
-    //     .attr("class", "x-axis")
-    //     .call(xAxis);
-    //
-    // svg.append("g")
-    //     .attr("class", "y-axis")
-    //     .call(yAxis);
+
+    svg.append("g")
+        .attr("class", "x-axis")
+        .attr("transform", `translate(0, ${height-margin.bottom})`)
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("class", "y-axis")
+        .call(yAxis);
 }
