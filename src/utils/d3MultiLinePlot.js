734e00bdabe366f86283867d5684707f13300383
Giulia Passoni <giulia@colour-profiling.com>
2023-07-22 18:49:42 +0100

    feat: fixed multiline plot.

--- src/utils/d3MultiLinePlot.js
+++ src/utils/d3MultiLinePlot.js
@@ -1,5 +1,19 @@
 import * as d3 from "d3"
-import {axisBottom, axisLeft, curveLinear, line, scaleLinear, select, group, extent, timeParse, scaleTime, scaleOrdinal, schemeCategory10} from "d3";
+import {
+    axisBottom,
+    axisLeft,
+    curveLinear,
+    line,
+    scaleLinear,
+    select,
+    group,
+    extent,
+    timeParse,
+    scaleTime,
+    scaleOrdinal,
+    schemeCategory10,
+    max
+} from "d3";
 
 export function d3MultiLineChart(myRef, width, height, margin, data, variableName) {
 
@@ -13,14 +27,32 @@ export function d3MultiLineChart(myRef, width, height, margin, data, variableNam
             "translate(" + margin.left + "," + margin.top + ")")
 
     console.log(data)
-    const xScale = scaleTime()
-        .domain(extent(data[0].gdp5yearsData, function(d) { return d.year}))
+    const xScale = scaleLinear()
+        .domain(extent(data[0].gdp5yearsData, d => d.year))
         .range([0, width - margin.right])
 
     const xAxis = axisBottom(xScale).ticks(5);
 
+    let fullYDomain = []
+    let minY = Infinity;
+    let maxY = -Infinity;
+    for (let i=0; i < data.length; i++) {
+        let item = data[i].gdp5yearsData
+        for (let j=0; j<item.length; j++) {
+            fullYDomain.push(item[j].gdpValue)
+
+            if (item[j].gdpValue < minY) {
+                minY = item[j].gdpValue
+            }
+
+            if (item[j].gdpValue > maxY) {
+                maxY = item[j].gdpValue
+            }
+        }
+    }
+
     const yScale  = scaleLinear()
-        .domain(extent(data[0].gdp5yearsData, function(d) { return d.gdpValue}))
+        .domain([minY, maxY])
         .range([height - margin.bottom, margin.top]) // it's inverted bc we always start from top left, then going downwards
 
     const yAxis = axisLeft(yScale).ticks(5);
@@ -28,29 +60,22 @@ export function d3MultiLineChart(myRef, width, height, margin, data, variableNam
     let color = scaleOrdinal(schemeCategory10);
 
     let myline = line()
-        .x((d)=> { xScale(d.year); })
-        .y((d)=> { yScale(d.gdpValue); });
+        // TODO: Note the syntax change. The code below wouldn't work, if we only sued the {} without the return keyword!
+        .x((d) => {return xScale(d.year)})
+        .y((d) => {return yScale(d.gdpValue)});
 
     let lines = svg.append('g')
         .attr('class', 'lines');
 
     lines.selectAll('.line-group')
-        .data([data]).enter()
+        .data(data).enter()
         .append('g')
         .attr('class', 'line-group')
         .append('path')
         .attr('class', 'line')
-        // TODO: fix line below to render graph correctly
-        .attr('d', d => myline(d.values))
+        .attr('d', d => myline(d.gdp5yearsData))
         .style('stroke', (d, i) => color(i))
-
-    // svg.append("path")
-    //     .data([data])
-    //     .attr("class", "line")
-    //     .attr("d", valueline)
-    //     .attr('fill', 'none')
-    //     .attr('stroke', 'black')
-
+        .attr('fill', 'none')
 
     svg.append("g")
         .attr("class", "x-axis")
